50beeb4f34d7c285ba3967ef03bd3e83e77c6f99
Shtrompel <40803431+Shtrompel@users.noreply.github.com>
2020-10-29 19:33:11 +0200

    Removed Warnings + ActivityProject Changes

--- app/src/androidTest/java/com/igalblech/school/graphicaljavascriptcompiler/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/igalblech/school/graphicaljavascriptcompiler/ExampleInstrumentedTest.java
@@ -8,20 +8,18 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-// --Commented out by Inspection START (28/10/2020 14:07):
-///**
-// * Instrumented test, which will execute on an Android device.
-// *
-// * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
-// */
-//@RunWith(AndroidJUnit4.class)
-//public class ExampleInstrumentedTest {
-//    @Test
-//    public void useAppContext ( ) {
-//        // Context of the app under test.
-//        Context appContext = InstrumentationRegistry.getInstrumentation ( ).getTargetContext ( );
-//
-//        assertEquals ( "com.igalblech.school.graphicaljavascriptcompiler", appContext.getPackageName ( ) );
-//    }
-//}
-// --Commented out by Inspection STOP (28/10/2020 14:07)
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext ( ) {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation ( ).getTargetContext ( );
+
+        assertEquals ( "com.igalblech.school.graphicaljavascriptcompiler", appContext.getPackageName ( ) );
+    }
+}
